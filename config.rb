59561c939e05899c8b42aa729b96de31bbe62c3b
guillaume crouillere <gcrouillere@hotmail.fr>
2017-05-08 17:17:15 +0200

    final

--- config.rb
+++ config.rb
@@ -89,3 +89,7 @@ activate :deploy do |deploy|
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
+data.flats.each do |owner, infos|
+  proxy "/flats/#{owner}.html", "/flats/show.html", locals: { owner: owner, infos: infos }, ignore: true
+end
